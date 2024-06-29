16fa74cbe38b924f1f097aa0a5c286a50178e3fc
Nishita0807 <saranj0903@gmail.com>
2024-06-29 17:44:36 +0530

    use back button to navigate to theme page from profile page

--- components/Profile.js
+++ components/Profile.js
@@ -279,7 +279,7 @@ const Profile = ({onMenuItemClick}) => {
       <ProfileHeader>
         <div className='box-1'>
           <div  onClick={() => onMenuItemClick('home')}><FaArrowLeft /></div>
-          <HeaderImage src={croppedImage} />
+          <HeaderImage src={croppedImage === null ?  '/images/person.jpeg' : croppedImage} />
           <h3>{name} {Lname}</h3>
         </div>
         <div className='date'>
@@ -297,7 +297,7 @@ const Profile = ({onMenuItemClick}) => {
         <div>
       <RoleLabel>PROFILE IMAGE</RoleLabel>
       <div>
-        <Portfolio src={croppedImage || '/images/person.jpeg'} />
+        <Portfolio src={croppedImage === null ?  '/images/person.jpeg' : croppedImage} />
       </div>
       <div className='image-portfolio'>
       
--- components/ThemeWindow.js
+++ components/ThemeWindow.js
@@ -7,14 +7,13 @@ const ThemeContainer = styled.div`
   padding: 20px;
   background-color: ${({ bgColor }) => bgColor};
   transition: background-color 0.3s;
-  
 `;
 
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  @media (max-width: 700px){
-  flex-direction:column;
+  @media (max-width: 700px) {
+    flex-direction: column;
   }
 `;
 
@@ -51,70 +50,63 @@ const TickIcon = styled(FaCheck)`
 
 const LandscapeContainer = styled.div`
   width: 100%;
-  height: 200px; /* or any other height */
+  height: 200px;
   overflow: hidden;
-  
-  
-  border:1px dashed black;
-    box-shadow: 12px 4px 8px ${({ lgradientColor }) => lgradientColor};
+  border: 1px dashed black;
+  box-shadow: 12px 4px 8px ${({ lgradientColor }) => lgradientColor};
   border-radius: 10px;
-  margin:12px;
-
+  margin: 12px;
 `;
 
 const LandscapeImage = styled.img`
-
-overflow-x: hidden;
-
+  overflow-x: hidden;
   width: 100%;
   height: 180px;
   object-fit: cover;
- margin:0px;
- border-radius:10px;
- margin-top:8px;
- margin-left:8px;
+  margin: 0px;
+  border-radius: 10px;
+  margin-top: 8px;
+  margin-left: 8px;
 `;
-const PersonImage = styled.img`
 
-width: 100px;
+const PersonImage = styled.img`
+  width: 100px;
   height: 100px;
   object-fit: cover;
-  border-radius:50%;
+  border-radius: 50%;
 `;
+
 const ThemeWindow = () => {
   const [bgColor, setBgColor] = useState('#fff');
-  const [lgradientColor, setlgradientColor] = useState('grey');
-  const colors = ['black', '#d9d9d9', 'white', '#c9e7b9', '#f3df98', '#8abcf7', '#eb9385', '#f9b4ee', '#9489f2', '#fef1ba'];
+  const [lgradientColor, setLgradientColor] = useState('grey');
+  const colors = ['black', '#d9d9d9', '#fff', '#c9e7b9', '#f3df98', '#8abcf7', '#eb9385', '#f9b4ee', '#9489f2', '#fef1ba'];
   const gradientc = ['#b3c2ab', '#a5a0c9', '#84dc67', '#74c5bb', '#9facbe', '#9eb4da', '#d8ab9e', '#d7b683', '#7fd6c6', '#de8ad6', '#a6859d', '#8680da', '#9d7ce6', '#dcd190'];
   const radialc = ['#8bb5c8', '#baa6d2', '#80e16c', '#80d3af', '#a9abbb', '#9fcfdf', '#deba9e', '#d8b782', '#82d2bc', '#de86d6', '#c886b5', '#917edc', '#a275e9', '#dece9a'];
-  const forec = ['black', 'white'];
+  const forec = ['black', '#fff'];
 
   return (
     <ThemeContainer bgColor={bgColor}>
-      
       <Header>
-        <div className='theme-header'>Themes</div>
-        <div className='theme-person'>
+        <div className="theme-header">Themes</div>
+        <div className="theme-person">
           <div>Kushagra Singh</div>
-          <div className='t-toggle'></div>
+          <div className="t-toggle"></div>
         </div>
       </Header>
-      <div className='profile'>
-        <div className='profile-content'>Apply a skin to your profile</div>
-        <div className='buttons'>
-          <div className='add'>
+      <div className="profile">
+        <div className="profile-content">Apply a skin to your profile</div>
+        <div className="buttons">
+          <div className="add">
             <div><FaPlus /></div>
             <div>Custom</div>
           </div>
-          <div className='save'>Save</div>
+          <div className="save">Save</div>
         </div>
       </div>
-      <div className='boxes'>
-        <div className='palette'>
+      <div className="boxes">
+        <div className="palette">
           <div>
-            <div className='color-palette'>
-              Solids
-            </div>
+            <div className="color-palette">Solids</div>
             <div>
               <ColorBox>
                 <ColorTemplate>
@@ -128,14 +120,12 @@ const ThemeWindow = () => {
             </div>
           </div>
           <div>
-            <div className='color-palette gradient'>
-              Linear Gradients
-            </div>
+            <div className="color-palette gradient">Linear Gradients</div>
             <div>
               <ColorBox>
                 <ColorTemplate>
                   {gradientc.map((color) => (
-                    <ColorPalette key={color} color={color} onClick={() => setlgradientColor(color)}>
+                    <ColorPalette key={color} color={color} onClick={() => setLgradientColor(color)}>
                       {lgradientColor === color && <TickIcon />}
                     </ColorPalette>
                   ))}
@@ -144,9 +134,7 @@ const ThemeWindow = () => {
             </div>
           </div>
           <div>
-            <div className='color-palette gradient'>
-              Radial Gradients
-            </div>
+            <div className="color-palette gradient">Radial Gradients</div>
             <div>
               <ColorBox>
                 <ColorTemplate>
@@ -160,9 +148,7 @@ const ThemeWindow = () => {
             </div>
           </div>
           <div>
-            <div className='color-palette gradient'>
-              Foreground Color
-            </div>
+            <div className="color-palette gradient">Foreground Color</div>
             <div>
               <ColorBox>
                 <ColorTemplate>
@@ -176,30 +162,27 @@ const ThemeWindow = () => {
             </div>
           </div>
         </div>
-        <div className='all-box'>
-          <div className='inner'>
-            <LandscapeContainer>
-              <LandscapeImage src="/images/landscape.jpg" alt='Landscape' />
-              
+        <div className="all-box">
+          <div className="inner">
+            <LandscapeContainer lgradientColor={lgradientColor}>
+              <LandscapeImage src="/images/landscape.jpg" alt="Landscape" />
             </LandscapeContainer>
             <div className="image">
-                <PersonImage src="/images/person.jpeg" alt="person"/>
+              <PersonImage src="/images/person.jpeg" alt="Person" />
+            </div>
+            <div className="profile-contents">
+              <h2>Mason Parker</h2>
+              <h5 className="heading">Graphics & UI/UX Designer</h5>
+              <h5 className="heading"><FaLocationArrow /> Denver, United States</h5>
+              <div className="content-box">
+                I am a visionary Graphics and UI/UX Designer, seamlessly blending artistic
+                flair with technical expertise. With a degree in Graphic Design, I
+                consistently deliver visually stunning and user-centric solutions, leaving an
+                indelible mark on the digital landscape. As a trusted collaborator, I am dedicated to crafting
+                pixel-perfect UIs and captivating graphics that elevate user experiences.
               </div>
-          <div className='profile-contents'>
-            <h2>Mason Parker</h2>
-            <h5 className='heading'>Graphics & UI/UX Designer</h5>
-            <h5 className='heading'><FaLocationArrow/> Denver, United State</h5>
-            <div className='content-box'>
-I am a viisonary Graphics and UI/UX Designer, seamlessly blending artistic
-falair with technical expertise. With a degree in Graphic Design. I 
-consistently deliver visually stunning and user-centric solutions, leaving an 
-indelible mark on the digital landscape. A a trusted collaborator, I am dedicated to crafting
-pixel-perfect UIs and captivating graphics that elevate user experiences.
+              <div className="bio">Biography</div>
             </div>
-            <div className='bio'>
-              Biography
-            </div>
-          </div>
           </div>
         </div>
       </div>
